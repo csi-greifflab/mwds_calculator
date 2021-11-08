b6be5ab7bbfbb88388848157b7a50ddc4754c76a
Jahn Zhong <93923632+jahnzh@users.noreply.github.com>
2021-11-08 15:06:02 +0100

    Initial repo

---
+++ ABC-EDA.py
@@ -0,0 +1,357 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 27 11:35:05 2021
+
+@author: Jahn Zhong
+"""
+import networkx as nx
+from random import choice, random, seed
+from collections import Counter
+import os
+import ray
+import time
+import numpy
+import sys
+import pandas as pd
+from operator import itemgetter
+
+
+def main():                  
+    #Setup
+    ray.init(num_cpus=8)
+    input_dir = 'fv'
+    graphs = get_graphs(input_dir)
+    edge_lists = get_edge_lists(graphs)
+    subgraphs = get_subgraphs(graphs, edge_lists)
+    """
+    beta:               proportion of nodes sampled from probability vector instead of previous generation for guided mutation
+    lmbda = 0.6
+    small_value:        probability of nodes not counted when initializing probability vector
+    population_size:    number of random solutions to evaluate
+    best_n:             n best solutions used for parent of next generation
+    max_iterations:     Number of iterations of algorithm
+    max_trials:
+    onlooker_bees: 
+    mu1:                probability of completely abandoning solution and generating a new one 
+    mu2:                closeness of new result during scout bee phase if not completely abandoned
+    """
+    graphs_with_isolated = get_graph_with_isolated(input_dir)
+    isolated_nodes = get_isolated_nodes(graphs_with_isolated)
+    print('calculating mds')
+    mds_dict = get_mds(graphs)
+    print('done')
+    doublets = get_doublets(graphs, mds_dict)
+    remove_doublets(graphs, mds_dict)
+    analyze(graphs, mds_dict, isolated_nodes, doublets, input_dir)
+    ray.shutdown()
+
+def get_subgraphs(graphs, edge_lists):
+    subgraphs = {}
+    component_sets = {}
+    for i in graphs:
+        component_sets[i] = {}
+        num = 1
+        for j in nx.connected_components(graphs[i]):
+            name = 'sub' + str(num)
+            if len(j) > 2:
+                component_sets[i][name] = j
+                num += 1
+    for k in component_sets:
+        subgraphs[k] = {}
+        for l in component_sets[k]:
+            subgraphs[k][l] = graphs[k].subgraph(nodes = component_sets[k][l])
+    return subgraphs
+
+def get_ds_with_isol(ds, isol):
+    ds_with_isol = {}
+    for key in ds:
+        ds_with_isol[key] = []
+        for node in ds[key]:
+            ds_with_isol[key].append(node)
+        for node in isol[key]:
+            ds_with_isol[key].append(node)
+    return ds_with_isol
+
+def get_doublets(graphs, ds):
+    doublets = {}
+    for graph in graphs:
+        doublets[graph] = []
+        for component in nx.connected_components(graphs[graph]):
+            if len(component) == 2:
+                doublets[graph].append(component)
+    return doublets
+
+def remove_doublets(graphs,ds):
+    for graph in graphs:
+        for component in nx.connected_components(graphs[graph]):
+            if len(component) == 2:
+                for node in component:
+                    if node in ds[graph]:
+                        ds[graph].remove(node)
+    
+    
+def get_edge_lists(graphs):
+    if type(graphs) is dict:
+        edge_lists = {graph: nx.to_pandas_edgelist(graphs[graph], source = 'Parm1', target = 'Parm2') for graph in graphs}
+        return edge_lists
+    else:
+        edge_list = nx.to_pandas_edgelist(graphs, source = 'Parm1', target = 'Parm2')
+        return edge_list
+
+def get_graphs(directory):
+    graphs = {}
+    for i in range(0,len(os.listdir(directory))): #iterate over files in directory
+        if os.listdir(directory)[i].endswith(".csv"):
+            graph_name = os.path.splitext(os.listdir(directory)[i])[0] #set name of graph as filename without extension
+            edge_list = pd.read_csv(str(directory + "/" + os.listdir(directory)[i])) #import edge list
+            edge_list = edge_list[edge_list['Parm1'] != edge_list['Parm2']]
+            G = nx.from_pandas_edgelist(edge_list, source = 'Parm1', target = 'Parm2', edge_attr = 'weight')
+            graphs[graph_name] = G
+    return graphs
+
+def get_graph_with_isolated(directory):
+    graphs = {}
+    for i in range(0,len(os.listdir(directory))): #iterate over files in directory
+        if os.listdir(directory)[i].endswith(".csv"):
+            graph_name = os.path.splitext(os.listdir(directory)[i])[0] #set name of graph as filename without extension
+            edge_list = pd.read_csv(str(directory + "/" + os.listdir(directory)[i])) #import edge list
+            G = nx.from_pandas_edgelist(edge_list, source = 'Parm1', target = 'Parm2', edge_attr = 'weight')
+            G.remove_edges_from(nx.selfloop_edges(G))
+            graphs[graph_name] = G
+    return graphs
+
+def get_isolated_nodes(graphs):
+    isol_nodes = {}
+    for i in graphs:
+        isol_nodes[i] = list(nx.isolates(graphs[i]))
+    return isol_nodes
+@ray.remote
+def abc_eda(
+        graph, edge_list, population_size, max_iterations, max_trials, 
+        onlooker_bees,  d, beta, lmbda, mu1, mu2, small_value):
+    seed()
+    best_n = int(round(population_size/d))
+    population, fitness_scores, best_solution = init_pop(graph, population_size, edge_list)
+    best_iteration = best_solution.copy()
+    probability_vector = init_prob_v(graph, population, best_n, small_value)
+    iterations = 1
+    while iterations <= max_iterations:
+        iterations += 1
+        population, best_iteration = emp_bee_phase(graph, population, edge_list, max_trials, best_iteration, probability_vector, mu1, mu2, population_size, beta)
+        population, best_iteration = onlooker_bee_phase(population, graph, edge_list, probability_vector, best_iteration, onlooker_bees, beta)
+        probability_vector = update_prob_v(population, probability_vector, edge_list, lmbda, best_n)
+        if fitness(best_iteration, edge_list) < fitness(best_solution, edge_list):
+            best_solution = best_iteration
+    return list(set(best_solution))
+
+def guided_mutation(
+        graph, 
+        solution, 
+        probability_vector, beta):
+    V = list(graph.nodes())
+    mutation = []
+    for node in V:
+        if random() < beta:
+            if random() < probability_vector[node]:
+                mutation.append(node)
+        elif node in solution:
+            mutation.append(node)
+    return mutation
+    
+def init_prob_v(graph, solutions, best_n, small_value):
+    V = list(graph.nodes())
+    solution_quality = {key: fitness(solutions[key], get_edge_lists(graph)) for key, value in solutions.items()}
+    tmp_dict = dict(sorted(solution_quality.items(),key = itemgetter(1))[:best_n])
+    best_n_solutions = {key: solutions[key] for key in tmp_dict.keys()}
+    list_of_occurence = []
+    for solution in best_n_solutions:
+        list_of_occurence += best_n_solutions[solution]
+    probability_vector = dict(Counter(list_of_occurence))
+    for node in probability_vector:
+        probability_vector[node] = probability_vector[node]/best_n
+    for node in V:
+        if node not in probability_vector.keys():
+            probability_vector[node] = float(small_value)
+    return probability_vector
+
+def update_prob_v(solutions, probability_vector, edge_list, lmbda, best_n):
+    solution_quality = {key: fitness(solutions[key], edge_list) for key, value in solutions.items()}
+    tmp_dict = dict(sorted(solution_quality.items(),key = itemgetter(1))[:best_n])
+    best_n_solutions = {key: solutions[key] for key in tmp_dict.keys()}
+    list_of_occurence = []
+    for solution in best_n_solutions:
+        list_of_occurence += best_n_solutions[solution]
+    occurences = dict(Counter(list_of_occurence))
+    for node in probability_vector:
+        if node in occurences.keys():
+            probability_vector[node] =  (1-lmbda)*probability_vector[node] + lmbda*(occurences[node]/best_n)
+    return probability_vector
+                                                                                    
+def fitness(solution, edge_list):
+    fitness_score = edge_list.loc[edge_list['Parm2'].isin(solution) + edge_list['Parm1'].isin(solution), 'weight'].sum()
+    return fitness_score
+
+def repair(graph, solution):
+    while nx.is_dominating_set(graph, solution) == False:
+        solution.append(random_heuristic_node(graph, solution))
+
+def improve(graph, solution):
+    R = []
+    for node in solution:
+        tmp = solution.copy()
+        tmp.remove(node)
+        if nx.is_dominating_set(graph, tmp) == True:
+            R.append(node)
+    while R != []:
+        solution.remove(heuristic_improv(graph, R))
+        R = []
+        for node in solution:
+            tmp = solution.copy()
+            tmp.remove(node)
+            if nx.is_dominating_set(graph, tmp) == True:
+                R.append(node)
+
+def init(graph):
+    solution = []
+    while nx.is_dominating_set(graph, solution) == False:
+        solution.append(choice(list(graph.nodes())))
+    return solution
+     
+def init_pop(graph, population_size, edge_list):
+    solutions = {}
+    fitness_scores = {}
+    solutions[0] = []
+    repair(graph, solutions[0])
+    solutions[1] = list(graph.nodes())
+    improve(graph, solutions[1])
+    for i in range(2, population_size):
+        solutions[i] = init(graph)
+    for solution in solutions:
+        fitness_scores[solution] = fitness(solutions[solution], edge_list)
+    best_solution = solutions[min(fitness_scores, key=fitness_scores.get)]
+    return solutions, fitness_scores, best_solution
+
+def emp_bee_phase(
+        graph, 
+        solutions, 
+        edge_list, 
+        max_trials, 
+        best_iteration, 
+        probability_vector, mu1, mu2, population_size, beta):
+    trials_with_no_change = 0
+    for i in range(0,population_size):
+        index = i
+        trials_with_no_change += 1
+        mutated_solution = guided_mutation(graph, solutions[index], probability_vector, beta)
+        repair(graph, mutated_solution)
+        improve(graph, mutated_solution)
+        if fitness(mutated_solution, edge_list) < fitness(solutions[index], edge_list):
+            solutions[index] = mutated_solution
+            trials_with_no_change = 0
+        elif trials_with_no_change == max_trials:
+            solutions = scout_bee_phase(graph, solutions, index, best_iteration, mu1, mu2)
+        if fitness(solutions[index], edge_list) < fitness(best_iteration, edge_list):
+            best_iteration = solutions[index]
+    return solutions, best_iteration
+
+def scout_bee_phase(graph, solutions, index, best_iteration, mu1, mu2):
+    if random() < mu1:
+        solutions[index] = init(graph)
+    else:
+        solutions[index] = []
+        for node in best_iteration:
+            if random() < mu2:
+                solutions[index].append(node)
+        repair(graph, solutions[index])
+    return solutions
+
+def onlooker_bee_phase(
+        solutions, graph, edge_list, probability_vector, best_iteration, onlooker_bees, beta):
+    for i in range(0,onlooker_bees):
+        index = choice(range(0,len(solutions)))
+        O = guided_mutation(graph, solutions[index], probability_vector, beta)
+        repair(graph, O)
+        improve(graph, O)
+        if fitness(O, edge_list) < fitness(best_iteration, edge_list):
+            best_iteration = O
+        if fitness(O, edge_list) < fitness(solutions[index], edge_list):
+            solutions[index] = O
+    return solutions, best_iteration
+
+def random_heuristic_node(graph, solution):
+    random_choice = choice([1,2,3])
+    heuristics = {1:{}, 2:{}, 3:{}}
+    V = [node for node in list(graph.nodes()) if node not in solution]
+    vertex = {}
+    dominated = []
+    dominated += solution
+    for node in solution:
+        dominated += list(graph.neighbors(node))
+    for node in V:
+        vertex[node] = {}
+        undominated_neighbors = [node for node in list(graph.neighbors(node)) if node not in dominated]
+        vertex[node]['weight_sum'] = graph.degree(node, 'weight')
+        vertex[node]['degree'] = graph.degree(node)
+        vertex[node]['weight_sum_of_non_dominated_neighbors'] = 0
+        for neighbor in undominated_neighbors:
+            vertex[node]['weight_sum_of_non_dominated_neighbors'] += graph[node][neighbor]['weight']
+    for node in vertex:
+        heuristics[1][node] = vertex[node]['degree']/vertex[node]['weight_sum']
+        heuristics[2][node] = vertex[node]['weight_sum_of_non_dominated_neighbors']/vertex[node]['weight_sum']
+        heuristics[3][node] = (vertex[node]['weight_sum_of_non_dominated_neighbors']*vertex[node]['degree'])/vertex[node]['weight_sum']
+    return max(heuristics[random_choice], key=heuristics[random_choice].get)
+
+def heuristic_improv(graph, solution):
+    heuristics = {1:{}}
+    V = solution
+    vertex = {}
+    for node in V:
+        vertex[node] = {}
+        vertex[node]['weight_sum'] = graph.degree(node, 'weight')
+        vertex[node]['degree'] = graph.degree(node)
+    for node in vertex:
+        heuristics[1][node] = vertex[node]['weight_sum']/vertex[node]['degree']
+    return max(heuristics[1], key=heuristics[1].get)
+
+def test_output(test_n, edge_list):
+    output = {}
+    overlaps = []
+    for i in range(test_n):
+        output[i] = abc_eda.remote(graph, edge_list, population_size, max_iterations, max_trials)
+    for i in range(test_n):
+        output[i] = ray.get(output[i])
+    for i in range(test_n):
+        for j in range(test_n):
+            if i != j:
+                overlaps.append(overlap(output[i], output[j]))
+    fitness_scores = [fitness(output[i], edge_list) for i in range(test_n)]
+    average_fitness = average(fitness_scores)
+    std_fitness = numpy.std(fitness_scores)
+    average_overlap = average(overlaps)
+    std_overlap = numpy.std(overlaps)
+    return output, average_overlap, std_overlap, average_fitness, std_fitness
+
+def intersection(set1, set2):
+    intersect = [vertex for vertex in set1 if vertex in set2]
+    return intersect
+
+def overlap(set1, set2):
+    return len(intersection(set1,set2))/((len(set1)+len(set2))/2)
+
+def average(lst):
+    return sum(lst)/len(lst)
+
+def get_mds(
+        graphs, population_size = 50, max_iterations = 20, max_trials = 25, 
+        onlooker_bees = 20, d = 5, beta = 0.2, lmbda = 0.6, mu1 = 0.5, mu2 = 0.5, 
+        small_value = 0.05):
+    edge_lists = get_edge_lists(graphs)
+    mds_dict = {graph: abc_eda.remote(
+                        graphs[graph], edge_lists[graph], population_size, max_iterations, max_trials,
+                        onlooker_bees,  d, beta, lmbda, mu1, mu2, small_value)
+        for graph in graphs}
+    mds_dict = {graph: ray.get(mds_dict[graph]) for graph in graphs}
+    return mds_dict
+  
+if __name__ == "__main__":
+    main()
